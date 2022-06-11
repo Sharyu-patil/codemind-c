5eb46be38e02dd502bfba1b9ac51382cc040ef34
Sharyu-patil <100119880+Sharyu-patil@users.noreply.github.com>
2022-06-11 15:47:27 +0530

    #include<stdio.h> int main() {     int n,arr[50],i,count=0,k;     scanf("%d%d",&n,&k);     for(i=0;i<n;i++)     scanf("%d",&arr[i]);     for(i=0;i<n;i++)     {         if(arr[i]%k==0)         {             count++;         }     }     printf("%d",count);     return 0; }

---
+++ Count_elements_divisible_by_k.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[50],i,count=0,k;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%k==0)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}    
