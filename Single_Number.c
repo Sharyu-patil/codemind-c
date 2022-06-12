6647aa0cc1af0768acf56f1be6a0947cfe857865
Sharyu-patil <100119880+Sharyu-patil@users.noreply.github.com>
2022-06-12 22:04:49 +0530

    #include<stdio.h> int main() {     int arr[1000],c,i,j,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;             }         }         if(c==1)         {             printf("%d",arr[i]);         }     }     return 0; }

---
+++ Single_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int arr[1000],c,i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            printf("%d",arr[i]);
+        }
+    }
+    return 0;
+}
