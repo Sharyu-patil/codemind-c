0a8e0784907e6ceb1c71e7ad0bf41ec3506730d4
Sharyu-patil <100119880+Sharyu-patil@users.noreply.github.com>
2022-07-03 10:23:13 +0530

    #include<stdio.h> int main() {     int a;;     scanf("%d",&a);     int i,j;     for(i=0;i<a;i++)     {         for(j=0;j<a-(i+1);j++)         {             printf(" ");         }         for(j=0;j<i*2+1;j++)         {             printf("%d",i+1);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int a;
+    int a;;
     scanf("%d",&a);
     int i,j;
     for(i=0;i<a;i++)
     {
-        for(j=1;j<a-1;j++)
+        for(j=0;j<a-(i+1);j++)
         {
-            printf("%d",j);
+            printf(" ");
         }
-        for(j=a-3;j>=1;j--)
+        for(j=0;j<i*2+1;j++)
         {
-            printf("%d",j);
+            printf("%d",i+1);
         }
         printf("
 ");
     }
-    return 0;
 }
